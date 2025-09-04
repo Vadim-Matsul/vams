0880377af28d5eb01dbe052d0f5f8dea7b43a9de
Vadim Vladimirovich Matsul <matsulmoscow@gmail.com>
2025-09-04 12:19:00 +0300

    14 Интеграция сохранения контактов в AMOcrm

--- src/app/home/HomePage.tsx
+++ src/app/home/HomePage.tsx
@@ -9,64 +9,64 @@ import { ContactBlock } from './ContactBlock'
 import { ContactForm } from './ContactForm'
 import { Footer } from '@/components/footer/Footer'
 import { StepperBlock } from './StepperBlock'
-import { ScrollUpMarginWrapper } from '@/ui/ScrollUpTranslateWrapper';
-import { cn } from '@/utils/cn';
-import { useEffect, useRef } from 'react';
-import Lenis from "lenis"
+// import { ScrollUpMarginWrapper } from '@/ui/ScrollUpTranslateWrapper';
+// import { cn } from '@/utils/cn';
+// import { useEffect, useRef } from 'react';
+// import Lenis from "lenis"
 
 type Props = {}
 
 
 export default function HomePage({ }: Props) {
 
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.3,
-      easing: t => 1 - Math.pow(1 - t, 3),
-      smoothWheel: true,
-    })
+  // useEffect(() => {
+  //   const lenis = new Lenis({
+  //     duration: 1.3,
+  //     easing: t => 1 - Math.pow(1 - t, 3),
+  //     smoothWheel: true,
+  //   })
 
-    let currentDir: 1 | -1 | 0 = 0
-    let animating = false
+  //   let currentDir: 1 | -1 | 0 = 0
+  //   let animating = false
 
-    const offScroll = lenis.on('scroll', (e: any) => {
-      currentDir = e.direction ?? 0
-      animating = Math.abs(e.velocity ?? 0) > 0.01
-    })
+  //   const offScroll = lenis.on('scroll', (e: any) => {
+  //     currentDir = e.direction ?? 0
+  //     animating = Math.abs(e.velocity ?? 0) > 0.01
+  //   })
 
-    const onWheel = (ev: WheelEvent) => {
-      const dy = ev.deltaY
-      if (!dy) return
-      const inputDir: 1 | -1 = dy > 0 ? 1 : -1
+  //   const onWheel = (ev: WheelEvent) => {
+  //     const dy = ev.deltaY
+  //     if (!dy) return
+  //     const inputDir: 1 | -1 = dy > 0 ? 1 : -1
 
-      if (animating && currentDir !== 0 && inputDir === -currentDir) {
-        lenis.stop()
+  //     if (animating && currentDir !== 0 && inputDir === -currentDir) {
+  //       lenis.stop()
 
-        try {
-          lenis.scrollTo(window.scrollY, { immediate: true })
-        } catch {
-        }
+  //       try {
+  //         lenis.scrollTo(window.scrollY, { immediate: true })
+  //       } catch {
+  //       }
 
-        requestAnimationFrame(() => {
-          lenis.start()
-        })
-      }
-    }
+  //       requestAnimationFrame(() => {
+  //         lenis.start()
+  //       })
+  //     }
+  //   }
 
-    window.addEventListener('wheel', onWheel, { passive: true })
+  //   window.addEventListener('wheel', onWheel, { passive: true })
 
-    function raf(time: number) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-    requestAnimationFrame(raf)
+  //   function raf(time: number) {
+  //     lenis.raf(time)
+  //     requestAnimationFrame(raf)
+  //   }
+  //   requestAnimationFrame(raf)
 
-    return () => {
-      window.removeEventListener('wheel', onWheel)
-      offScroll?.()
-      lenis.destroy()
-    }
-  }, [])
+  //   return () => {
+  //     window.removeEventListener('wheel', onWheel)
+  //     offScroll?.()
+  //     lenis.destroy()
+  //   }
+  // }, [])
 
 
   return (
--- src/server/routers/amo.ts
+++ src/server/routers/amo.ts
@@ -18,17 +18,45 @@ export const amoRouter = router({
       }),
     )
     .mutation(async ({ input }) => {
-      console.log(input);
-      const { name, ...custom_fields_values } = input;
-
       try {
-        const response = await amoRequest('/contacts', 'POST', [
+        const createResponse = await amoRequest('/contacts', 'POST', [
           {
-            name,
-            custom_fields_values,
+            name: input.name,
+            custom_fields_values: [
+              {
+                field_id: 204027, // phone
+                values: [{ value: input.phone, enum_id: 119089 }],
+              },
+              {
+                field_id: 259595,
+                values: [{ value: input.agreePolitics }], // agreePolitics
+              },
+            ],
           },
         ]);
-        return response;
+
+        console.log({ createResponse });
+        const contactId = createResponse?._embedded?.contacts?.[0]?.id;
+        console.log('contactId ', contactId);
+
+        if (!contactId) throw new Error('Не удалось получить id контакта');
+
+        // Создание заметки к контакту
+        const noteResponse = await amoRequest(`/contacts/${contactId}/notes`, 'POST', [
+          {
+            note_type: 'common',
+            params: {
+              text:
+                `Согласие на маркетинг: ${input.agreeMarketing ? 'ДА' : 'НЕТ'}; ` +
+                `Способ связи: Звонок-${input.contactType.call ? 'ДА' : 'НЕТ'}, ` +
+                `Telegram-${input.contactType.telegram ? 'ДА' : 'НЕТ'}, ` +
+                `WhatsApp-${input.contactType.whatsapp ? 'ДА' : 'НЕТ'};`,
+            },
+          },
+        ]);
+        console.log({ noteResponse });
+
+        return noteResponse;
       } catch (err) {
         console.error(`Ошибка addContact`, err);
         throw new Error('unknown error');
